3a8827749ffbd28eb5a13946a8d88796cc9bfcc4
VinCoD <5895.2020@students.ku.ac.ke>
2022-03-17 00:05:25 +0300

    Display sum of multiples of 5 between 1 and 100- full changes

--- src/WhileMultiplesFive.java
+++ src/WhileMultiplesFive.java
@@ -1,10 +1,11 @@
 class WhileMultiplesFive {
     public static void main(String[] args){
         int i =0;
-        int sum =0;
+        int sum = 0;
         while(i<=100){
-            System.out.println(i);
-            i+=5;
+            sum += i;
+            i += 5;
         }
+        System.out.println(sum);
     }
 }
