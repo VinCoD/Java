d14e77f520efeebdc7d67c15116fbb154ee6509d
VinCoD <5895.2020@students.ku.ac.ke>
2022-03-16 23:51:12 +0300

    Multiples of 5 between 1 and 100

---
+++ src/ForLoopMultiplesFive.java
@@ -0,0 +1,10 @@
+class ForLoopMultiplesFive {
+    public static void main(String[] args){
+        int i;
+        int sum =0;
+        for(i=0; i<=100; i+=5){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
