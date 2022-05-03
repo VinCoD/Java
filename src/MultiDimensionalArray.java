5e3acaf2e2e54b7d243c62b0ce78a0030129ce59
VinCoD <5895.2020@students.ku.ac.ke>
2022-05-03 14:08:27 +0300

    Multidimensional Array

---
+++ src/MultiDimensionalArray.java
@@ -0,0 +1,10 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args){
+        int i, j, first[][], second[][], sum[][];
+        for(i=0; i<4; i++){
+            for(j=0; j<5; j++){
+
+            }
+        }
+    }
+}
