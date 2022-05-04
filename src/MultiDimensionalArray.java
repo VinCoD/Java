5d630c2b8f808d6e9de4206ad3b0fdfabd92100b
VinCoD <5895.2020@students.ku.ac.ke>
2022-05-04 17:04:00 +0300

    Managed to write a program for accepting mulidimensional arrays

--- src/MultiDimensionalArray.java
+++ src/MultiDimensionalArray.java
@@ -1,10 +1,26 @@
+import javax.swing.*;
+
 public class MultiDimensionalArray {
     public static void main(String[] args){
-        int i, j, first[][], second[][], sum[][];
-        for(i=0; i<4; i++){
-            for(j=0; j<5; j++){
+        int i, j;
+        int first[][] = new int[10][10];
+        int second[][] = new int[10][10];
+        int sum[][] = new int[10][10];
+        for(i=0; i<2; i++){
+            for(j=0; j<2; j++){
+                first[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Enter values"));
+
 
             }
+            System.out.println();
+        }
+        for(i=0; i<2; i++){
+            for(j=0; j<2; j++){
+                System.out.print(first[i][j]);
+
+
+            }
+            System.out.println();
         }
     }
 }
